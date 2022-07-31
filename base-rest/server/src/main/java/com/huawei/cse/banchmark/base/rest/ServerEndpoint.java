bb6e02109d831fcead6d93df296de70501e11cc3
liubao <bismy@qq.com>
2022-07-31 18:28:08 +0800

    增加可靠性测试用例

--- base-rest/server/src/main/java/com/huawei/cse/banchmark/base/rest/ServerEndpoint.java
+++ base-rest/server/src/main/java/com/huawei/cse/banchmark/base/rest/ServerEndpoint.java
@@ -5,11 +5,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.netflix.config.DynamicPropertyFactory;
+
 @RestSchema(schemaId = "ServerEndpoint")
 @RequestMapping(path = "/")
 public class ServerEndpoint {
   @PostMapping(path = "/body")
   public String body(@RequestBody String body) {
+    int sleep = DynamicPropertyFactory.getInstance().getIntProperty("test.sleep.time", 0).get();
+    if (sleep > 0) {
+      try {
+        Thread.sleep(sleep);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
     return body;
   }
 }
--- tool/src/main/java/com/huawei/cse/banchmark/tool/ToolMain.java
+++ tool/src/main/java/com/huawei/cse/banchmark/tool/ToolMain.java
@@ -3,53 +3,88 @@ package com.huawei.cse.banchmark.tool;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.servicecomb.foundation.common.utils.BeanUtils;
 import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
 import org.apache.servicecomb.provider.springmvc.reference.RestTemplateBuilder;
+import org.apache.servicecomb.swagger.invocation.exception.InvocationException;
 import org.springframework.web.client.RestTemplate;
 
 public class ToolMain {
   private static RestTemplate template = RestTemplateBuilder.create();
 
+  static AtomicLong success = new AtomicLong(0);
+
+  static AtomicLong timeout = new AtomicLong(0);
+
+  static AtomicLong error = new AtomicLong(0);
+
   public static void main(String[] args) throws Exception {
     Log4jUtils.init();
     BeanUtils.init();
 
-    runTest(10, "1234567890");
+    runTest(40, "1234567890");
   }
 
   private static void runTest(int threadCount, String message) throws Exception {
     // initialize
-    ExecutorService executor = Executors.newFixedThreadPool(threadCount);
-    CountDownLatch latch = new CountDownLatch(threadCount);
-    for (int i = 0; i < threadCount; i++) {
-      executor.submit(() -> {
-        runTest(message);
-        latch.countDown();
-      });
-    }
-    latch.await();
+    ExecutorService executor = Executors.newFixedThreadPool(threadCount, new ThreadFactory() {
+      AtomicLong count = new AtomicLong(0);
 
-    // run 
+      @Override
+      public Thread newThread(Runnable r) {
+        return new Thread(r, "test-thread" + count.getAndIncrement());
+      }
+    });
+    int count = 100;
+    CountDownLatch latch = new CountDownLatch(threadCount * count);
+//    for (int i = 0; i < threadCount; i++) {
+//      executor.submit(() -> {
+//        runTest(message);
+//        latch.countDown();
+//      });
+//    }
+//    latch.await();
+
+    // run
+    long begin = System.currentTimeMillis();
     for (int i = 0; i < threadCount; i++) {
       executor.submit(() -> {
-        while (true) {
+        for (int j = 0; j < count; j++) {
           runTest(message);
+          latch.countDown();
         }
       });
     }
+
+    latch.await();
+
+    System.out.println("==========================================");
+    System.out.println(System.currentTimeMillis() - begin);
+    System.out.println(success.get());
+    System.out.println(timeout.get());
+    System.out.println(error.get());
+    System.out.println("==========================================");
   }
 
   private static void runTest(String message) {
     try {
       String result = template.postForObject("cse://client/body", message, String.class);
       if (!message.equals(result)) {
-        throw new RuntimeException("error");
+        error.incrementAndGet();
+      } else {
+        success.incrementAndGet();
       }
-    } catch (RuntimeException e) {
-      e.printStackTrace();
-      throw e;
+    } catch (Throwable e) {
+      if (e instanceof InvocationException) {
+        if (((InvocationException) e).getStatusCode() == 408) {
+          timeout.incrementAndGet();
+          return;
+        }
+      }
+      error.incrementAndGet();
     }
   }
 }
